3a6445be4eab0181535350c8842dace63270e1f4
huscker <egostalker03@mail.ru>
2022-08-20 00:53:15 +0300

    Added exceptions

---
+++ app/utils/exceptions.py
@@ -0,0 +1,35 @@
+from fastapi import status
+from typing import Any, Mapping, Optional
+from fastapi import status
+
+
+class CommonException(Exception):
+    def __init__(self, code: int, message: str, error: Exception | None) -> None:
+        super().__init__()
+        self.error = error
+        self.code = code
+        self.message = message
+
+class BadRequest(CommonException):
+    def __init__(self, message: str, error: Exception) -> None:
+        super().__init__(status.HTTP_400_BAD_REQUEST, message, error)
+
+class NotFoundException(CommonException):
+    def __init__(self, message: str) -> None:
+        super().__init__(status.HTTP_404_NOT_FOUND, message, None)
+
+class UserNotFoundException(NotFoundException):
+    def __init__(self) -> None:
+        super().__init__('User not found')
+
+class ItemNotFoundException(NotFoundException):
+    def __init__(self) -> None:
+        super().__init__('Item not found')
+
+class InternalServerError(CommonException):
+    def __init__(self, error: Exception) -> None:
+        super().__init__(status.HTTP_500_INTERNAL_SERVER_ERROR, 'Internal server error', error)
+
+class ForbiddenException(CommonException):
+    def __init__(self, message: str) -> None:
+        super().__init__(status.HTTP_403_FORBIDDEN, message, None)
