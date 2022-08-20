a639f3456ba6218aeaa13639b0e12762d68da9df
huscker <egostalker03@mail.ru>
2022-08-20 06:05:31 +0300

    Updated initial script

--- app/migrations/mongo/init-mongo.sh
+++ app/migrations/mongo/init-mongo.sh
@@ -3,10 +3,11 @@ set -e
 mongo <<EOF
 use $MONGO_INITDB_DATABASE
 
-db.createCollection('standarts', {
-  autoIndexId: true,
-})
-db.createCollection('items', {
-  autoIndexId: true,
-})
+db.createCollection('references')
+db.createCollection('items')
+
+db.references.createIndex({'product_id': 1},{unique: true})
+db.references.createIndex({'name': 'text'})
+db.items.createIndex({'product_id': 1},{unique: true})
+db.items.createIndex({'name': 'text'})
 EOF
