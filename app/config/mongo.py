6f021456f67b85a0a37e5eb2279fbda48240339e
huscker <egostalker03@mail.ru>
2022-08-20 06:04:30 +0300

    Added mongo support

--- app/config/mongo.py
+++ app/config/mongo.py
@@ -9,12 +9,6 @@ class MongoSettings(BaseSettings):
     class Config:
         env_file: str = '.env'
 
-    # @property
-    # def db_uri(self) -> str:
-    #     return PostgresDsn.build(
-    #         scheme="mongo",
-    #         user=self.MONGO_USER,
-    #         password=self.MONGO_PASSWORD,
-    #         host=self.MONGO_HOST,
-    #         path=f'/{self.MONGO_DATABASE}',
-    #     )
+    @property
+    def db_uri(self) -> str:
+        return f'mongodb://{self.MONGO_USER}:{self.MONGO_PASSWORD}@{self.MONGO_HOST}'
---
+++ app/services/mongo.py
@@ -0,0 +1,21 @@
+from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
+from app.utils.exceptions import InternalServerError
+from app.utils.get_settings import get_mongo_settings
+from app.utils.logger import Log
+
+class Mongo:
+    client: AsyncIOMotorDatabase
+    db: AsyncIOMotorDatabase
+
+    @classmethod
+    def connect_db(cls) -> None:
+        settings = get_mongo_settings()
+        try:
+            cls.client = AsyncIOMotorClient(settings.db_uri)
+            cls.db = cls.client[settings.MONGO_DATABASE]
+        except Exception as e:
+            raise InternalServerError(e) from e
+
+    @classmethod
+    def disconnect_db(cls) -> None:
+        cls.client.close()
